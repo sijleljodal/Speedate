384de3b76b6dc72480790170f071468cd36ee276
Silje Ljødal <silje.ljodal@gmail.com>
2020-02-13 14:10:22 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,102 @@
+
+<script>
+
+let side1Bobble1 = false
+let flaske 
+
+let side1 = false
+
+const animateFlaske = () => {
+	console.log('animate', flaske)
+	flaske.src='./flaskeanim.gif'
+}
+
+let startShow = false
+</script>
+
+
+<main>
+<section class='doors'>
+<img on:click={()=> side1=!side1} src='https://i.pinimg.com/originals/b0/f3/60/b0f36062c5eef964aab385b4fc60104d.png' alt='tre1'>
+
+<img src='https://i.pinimg.com/originals/b0/f3/60/b0f36062c5eef964aab385b4fc60104d.png' alt='tre1'>
+</section>
+
+<section class='dates'>
+<section class="side1">
+	<img on:click={() => side1Bobble1 = !side1Bobble1} 
+	src='https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/old-man-greg-joens.jpg' alt='bilde'>
+	
+	{#if side1Bobble1}
+		<img 
+		class='snakkeboble' 
+		src='https://images.twinkl.co.uk/tr/image/upload/illustation/How-Pink-Speech-Bubble--Icon-Cartoon-Comic-MPS-KS2-bw-RGB.png'
+		style='position: absolute;top:10rem; left:60%;' 
+		alt='snak'>
+	{/if}
+	<img 
+		on:click={animateFlaske}
+		bind:this={flaske}  
+		src= './flaske.png'
+		style='
+		position: absolute;
+		top: 20vh;
+		left: 20vh;
+		'
+		alt='pissss'
+	>
+
+</section>
+
+<section class="side2">2</section>
+<section class="side3">3</section>
+
+</section>
+</main>
+
+
+
+<style>
+
+
+main{
+margin: 0;
+padding: 0;
+}
+
+section{
+	width: 100vw;
+	height: 100vh;
+	scroll-snap-align: start;
+	scroll-snap-stop: always;
+	display: grid;
+	place-items: center;
+	position: relative;
+}
+
+.doors{
+	width: 100vw;
+	display: grid;
+	grid-auto-flow: column;
+}
+
+.dates{
+	height: 100vh;
+	display: grid;
+	grid-auto-flow: column;
+	overflow: scroll;
+	scroll-snap-type: x mandatory;
+}
+
+section img{
+	width: 30vw;
+}
+
+section:nth-of-type(even){
+	background-color: antiquewhite;
+} 
+:global(html, body){
+	margin: 0;
+	padding: 0;
+}
+</style>
---
+++ src/Happening.svelte
@@ -0,0 +1,21 @@
+	  <script>
+      import { slide } from 'svelte/transition'
+      export let name
+      </script>
+      
+      <div class='happening' in:slide out:slide>
+		 <h2>Its happening!{name}</h2>
+	  </div>
+
+
+      <style>
+
+      	.happening{
+		position:fixed;
+		top:3rem;
+		right:3rem;
+		padding:2rem;
+		background: #fec1eb;
+		color: #ff00ae;
+	}
+      </style>
